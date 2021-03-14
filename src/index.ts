f54068e92d8735977e80e5fa33c810eb4c9c1637
jaredgorski <jared.gorski@liferay.cloud>
2021-03-13 17:52:44 -0800

    Fix export

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export { default } from './files';
 
-export { default as Files } from './file';
+export { default as Files } from './files';
 export { default as File } from './file';
 
 export { regex } from './file/utils/regex';
